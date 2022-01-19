3fdbebe968c8735fbebec576b0104ea9150fec99
nusbog <86577152+nusbog@users.noreply.github.com>
2022-01-19 22:18:49 +0100

    API call
    Kan nu få information från väder-API

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace WeatherAPIConsole
+{
+    class Program
+    {
+        static void Get()
+        {
+            string response;
+            using (WebClient client = new WebClient())
+            {
+                response = client.DownloadString("http://api.openweathermap.org/data/2.5/weather?q=Stockholm&appid=c6a2fb09b303bc24611ed5f3b3ff67d4");
+            }
+
+            File.WriteAllText("../../../response.json", response);
+        }
+
+        static void Main(string[] args)
+        {
+            Get();
+        }
+    }
+}
