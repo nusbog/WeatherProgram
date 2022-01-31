a12d76c05c8d4053e82590de5127747c587f8131
nusbog <86577152+nusbog@users.noreply.github.com>
2022-01-31 10:03:17 +0100

    Parse JSON till klasser
    Info från API-call skickas nu till klasser

--- Program.cs
+++ Program.cs
@@ -1,25 +1,93 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace WeatherAPIConsole
 {
+    public class Coord
+    {
+        public double lon { get; set; }
+        public double lat { get; set; }
+    }
+
+    public class Weather
+    {
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+    }
+
+    public class Main
+    {
+        public double temp { get; set; }
+        public double feels_like { get; set; }
+        public double temp_min { get; set; }
+        public double temp_max { get; set; }
+        public int pressure { get; set; }
+        public int humidity { get; set; }
+    }
+
+    public class Wind
+    {
+        public double speed { get; set; }
+        public int deg { get; set; }
+    }
+
+    public class Clouds
+    {
+        public int all { get; set; }
+    }
+
+    public class Sys
+    {
+        public int type { get; set; }
+        public int id { get; set; }
+        public string country { get; set; }
+        public int sunrise { get; set; }
+        public int sunset { get; set; }
+    }
+
+    public class Root
+    {
+        public Coord coord { get; set; }
+        public List<Weather> weather { get; set; }
+        public string @base { get; set; }
+        public Main main { get; set; }
+        public int visibility { get; set; }
+        public Wind wind { get; set; }
+        public Clouds clouds { get; set; }
+        public int dt { get; set; }
+        public Sys sys { get; set; }
+        public int timezone { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public int cod { get; set; }
+    }
+
     class Program
     {
         static void Get()
         {
             string response;
+
             using (WebClient client = new WebClient())
             {
                 response = client.DownloadString("http://api.openweathermap.org/data/2.5/weather?q=Stockholm&appid=c6a2fb09b303bc24611ed5f3b3ff67d4");
             }
 
-            File.WriteAllText("../../../response.json", response);
+            Root myDeserializedClass = JsonSerializer.Deserialize<Root>(response);
+            Console.WriteLine(myDeserializedClass.coord.lat);
         }
 
-        static void Main(string[] args)
+        static void Main()
         {
             Get();
         }
     }
+
+
 }
