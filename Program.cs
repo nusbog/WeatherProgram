c3bba3fd11e58bb9c947bcb217034718a7843d62
nusbog <henryrosenlund10@gmail.com>
2022-03-09 15:32:18 +0100

    Final Commit

--- Program.cs
+++ Program.cs
@@ -1,31 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Net;
+﻿using System.Globalization;
 using System.Text.Json;
-using System.Text.Json.Serialization;
+using System.Net;
+using System.IO;
+using System;
 
 namespace WeatherAPIConsole
 {
     class Program
     {
-        static void Get(string city)
+        static string key = "";
+        static bool keyIsSet = false;
+
+        static void SetKey(string keyPath) {
+            // Kolla om sökvägen till API-nyckeln finns
+            try {
+                key = File.ReadAllText(keyPath);
+                keyIsSet = true;
+            } catch {
+                Console.WriteLine(keyPath + " finns inte eller är inte en API-nyckel, vänligen specificera sökvägen till din API-nyckel: ");
+                SetKey(Console.ReadLine());
+            }
+        }
+
+        static WeatherData.Root Get(string city)
         {
             string response;
-            string key = "";
 
             using (WebClient client = new WebClient())
             {
-                string url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key;
-                response   = client.DownloadString(url);
+                string url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key + "&units=metric";
+
+                // Kolla om data för staden existerar
+                try
+                {
+                    response = client.DownloadString(url);
+                } catch {
+                    Console.WriteLine('\"' + city + "\" är ingen stad");
+                    return null;
+                }
             }
 
             WeatherData.Root data = JsonSerializer.Deserialize<WeatherData.Root>(response);
+            return data;
+        }
+
+        static void WriteData(string city, WeatherData.Root data) {
+            string writeData =
+                "--- " + city.ToUpper() + ", " + data.sys.country.ToUpper() + " ---\n" +
+                "lon: " + data.coord.lon + " lat: " + data.coord.lat + "\n" +
+                "temp: " + data.main.temp + " °C\n" +
+                "feels like: " + data.main.feels_like + " °C\n" +
+                "humidity: " + data.main.humidity + "\n" +
+                "pressure: " + data.main.pressure + "\n" +
+                "wind speed: " + data.wind.speed + "\n";
+            
+            Console.WriteLine(writeData);
+
+            // Filhantering
+            string date = DateTime.Today.ToString().Replace(" 00:00:00", ""); // Få endast datum, ej tid
+            File.WriteAllText(city + '_' + date + ".txt", writeData); // Ex: Stockholm_2022-01-01
         }
 
         static void Main()
         {
+            // Läs inte in nyckeln om den redan är inläst
+            if(!keyIsSet)
+                SetKey("key.txt");
+
+            Console.WriteLine("Skriv stad: ");
             string city = Console.ReadLine();
+            WeatherData.Root data = Get(city);
+
+            if(data != null) {
+                WriteData(city, data);
+            }
+
+            Main();
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,30 @@
+# Weather Program
+
+This program is written in C# and is intended to fetch weather data from [OpenWeatherMap](https://openweathermap.org/api). 
+
+## Usage
+
+For the program to work, an API-key from [OpenWeatherMap](https://openweathermap.org/api) needs to be put in the projects build directory or assigned during runtime.
+When the program starts, you will need to specify what location to fetch the data from. OpenWeatherMap's API will then fetch the data and print it out in the console and write to a file in the projects build directory.
+
+## Installation
+
+First clone the repository
+
+```cmd
+git clone https://github.com/nusbog/WeatherProgram
+```
+
+Compile and run the program
+
+```cmd
+dotnet run
+```
+
+## Contribution
+
+No pull requests are currently allowed.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
